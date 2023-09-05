b3b236401355f68ccf39e71be5e7c68d816f4376
Furaha Emile <89402664+furahaemire@users.noreply.github.com>
2023-09-05 10:53:55 +0000

    changes code

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -11,14 +11,14 @@ function About() {
   return (
     <div>
       <Navbar />
-      <div class="row">
-        <div class="right">
+      <div class="right">
+        <div class="container ml-4">{/* about me section */}
+          <h3 class="mb-2 text-3xl font-medium leading-tight text-primary  mt-10">
+            <b>ABOUT<b class="text-cyan-400 font-black"> ME </b></b>
+          </h3>
 
-          <div class="col3">
-            <h3 class="mb-2 text-3xl font-medium leading-tight text-primary ml-4 mt-10">
-              <b>ABOUT<b class="text-cyan-400 font-black"> ME </b></b>
-            </h3>
-            <p class="ml-4 mr-4">I am self-taught, but I honestly
+          <div class="card">
+            <p class="">I am self-taught, but I honestly
               believe that with the amount of tutorials on YouTube and other online platform, one can
               learn everything–including HTML, CSS and PHP, react, and other technology skills without
               going to college. I’ve tried to make my living as a freelance front end web developer,
@@ -27,28 +27,64 @@ function About() {
             </p>
           </div>
 
-          <div class="col4  mt-20">
-            <div class="mb-13">
-              <p class="text-black"> <b class="text-cyan-400 font-black ml-2 mr-4">Resident</b>Rwanda</p>
-              <p class="text-black"> <b class="text-cyan-400 font-black ml-2 mr-4">Adress</b> Kigali-Rwanda</p>
-              <p class="text-black"> <b class="text-cyan-400 font-black ml-2 mr-4">Email</b>furahaemire5@gmail.com</p>
-              <p class="text-black"> <b class="text-cyan-400 font-black ml-2 mr-4">Phone</b>+250785911888</p>
-            </div>
-            <b />
-
+          <div class="card">
+            <p class="text-black"> <b class="text-cyan-400 font-black ml-2 mr-4">Resident</b>Rwanda</p>
+            <p class="text-black"> <b class="text-cyan-400 font-black ml-2 mr-4">Adress</b> Kigali-Rwanda</p>
+            <p class="text-black"> <b class="text-cyan-400 font-black ml-2 mr-4">Email</b>furahaemire5@gmail.com</p>
+            <p class="text-black"> <b class="text-cyan-400 font-black ml-2 mr-4">Phone</b>+250785911888</p>
           </div>
         </div>
 
 
+        <h3 class="mb-2 text-3xl font-medium leading-tight text-primary ml-4 mt-10 mb-8">
+          <b>WHAT<b class="text-cyan-400 font-black"> I DO </b></b>
+        </h3>
+       
+        <div class="container ml-4"> {/* what i do  */}
+        <div class="card">
+        <IconContext.Provider value={{ size: '2.5rem', color: '#7dd3fc', }}>
+                <GiGreenhouse />
+                <p class="text-xl font-black mt-2  mb-2">Ecommerce</p>
+                <p>Ecommerce web development is the process of building an ecommerce website.
+                  Frontend and backend development for ecommerce sites include checkout systems,
+                  payment processing, site security, and more.
+                </p>
+              </IconContext.Provider>
+          </div>
 
+          <div class="card">
+          <IconContext.Provider value={{ size: '2.5rem', color: '#7dd3fc', }}>
+                <CgWebsite />
+                <p class="text-xl font-black mt-2  mb-2">Website</p>
+                <p>Ecommerce web development is the process of building an ecommerce website.
+                  Frontend and backend development for ecommerce sites include checkout systems,
+                  payment processing, site security, and more.
+                </p>
+              </IconContext.Provider>
+          </div>
 
+          <div class="card">
+          <IconContext.Provider value={{ size: '2.5rem', color: '#7dd3fc', }}>
+                <MdOutlineComputer />
+                <p class="text-xl font-black mt-2  mb-2">Ecommerce</p>
+                <p>Ecommerce web development is the process of building an ecommerce website.
+                  Frontend and backend development for ecommerce sites include checkout systems,
+                  payment processing, site security, and more.
+                </p>
+              </IconContext.Provider>
+          </div>
 
-
-
-
-
-
-
+          <div class="card">
+          <IconContext.Provider value={{ size: '2.5rem', color: '#7dd3fc', }}>
+                <CiMobile3 />
+                <p class="text-xl font-black mt-2  mb-2">Mobile App</p>
+                <p>Ecommerce web development is the process of building an ecommerce website.
+                  Frontend and backend development for ecommerce sites include checkout systems,
+                  payment processing, site security, and more.
+                </p>
+              </IconContext.Provider>
+          </div>
+        </div>
 
       </div>
     </div>
