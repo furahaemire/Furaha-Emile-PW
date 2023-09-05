2e5aa01ce7e92d0f5ee01008f7f44c80c956ccc4
Furaha Emile <89402664+furahaemire@users.noreply.github.com>
2023-09-05 08:45:18 +0000

    change my name

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <div class="right">
         <div class="home">
           <h1 class="mb-2 mt-0 text-5xl font-medium leading-tight text-primary">
-            <b>FURAHA EMILE</b>
+            <b>FURAHA EMILE...</b>
           </h1>
           <p className="mb-4 mt-6 text-xl font-light leading-relaxed">
             Frontend-Developer
